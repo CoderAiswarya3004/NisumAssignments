f51bc05efeb630e1a0e529b8fa89519fe22845d5
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-09 19:21:11 +0530

    Create q14.java

---
+++ Sprint 1 Day1/q14.java
@@ -0,0 +1,7 @@
+public class IntegerToFloatConversion {
+    public static void main(String[] args) {
+        Integer num = 50;
+        Float f = num.floatValue();
+        System.out.println("Float value: " + f);
+    }
+}
