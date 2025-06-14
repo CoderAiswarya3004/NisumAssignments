4d61c0adfd709c51589bb93a22a451289d186c5b
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-14 12:57:34 +0530

    Create  PageTwoServlet .java

---
+++ Sprint 1 Day 6/q14/PageTwoServlet .java
@@ -0,0 +1,20 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class PageTwoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String age = request.getParameter("age");
+        HttpSession session = request.getSession();
+        String name = (String) session.getAttribute("name");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<h2>Form Summary</h2>");
+        out.println("<p>Name: " + name + "</p>");
+        out.println("<p>Age: " + age + "</p>");
+    }
+}
