fddacaa9986100eb701bae6c9b069632045f8a38
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-22 00:21:36 +0530

    Update q13.java

--- Sprint 2 Day 2/q13.java
+++ Sprint 2 Day 2/q13.java
@@ -1,10 +1,10 @@
 public class Q13 {
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>(List.of(
-            new Employee("Jubu", "Male", 50000),
+            new Employee("Jitun", "Male", 50000),
             new Employee("Alica", "Female", 60000),
             new Employee("Micky", "Male", 70000),
-            new Employee("Lile", "Female", 55000)
+            new Employee("Lily", "Female", 55000)
         ));
 
         employees.sort(Comparator.comparingDouble(e -> e.salary));
