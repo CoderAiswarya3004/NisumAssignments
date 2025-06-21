6d026a3d4552d59352373bb6d0fae87b4272dbaf
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-21 18:20:51 +0530

    Create q16.java

---
+++ Sprint 2 Day 1/q16.java
@@ -0,0 +1,16 @@
+@FunctionalInterface
+interface MathOperation {
+    int operate(int a, int b);
+}
+
+public class MathLambdaDemo {
+    public static void main(String[] args) {
+        MathOperation add = (a, b) -> a + b;
+        MathOperation subtract = (a, b) -> a - b;
+        MathOperation multiply = (a, b) -> a * b;
+
+        System.out.println("Add: " + add.operate(5, 3));
+        System.out.println("Subtract: " + subtract.operate(5, 3));
+        System.out.println("Multiply: " + multiply.operate(5, 3));
+    }
+}
