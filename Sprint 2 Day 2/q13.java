a3907379db3d20ca8a457ec981dd85e74ae97874
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-22 00:20:07 +0530

    Create q13.java

---
+++ Sprint 2 Day 2/q13.java
@@ -0,0 +1,16 @@
+public class Q13 {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>(List.of(
+            new Employee("Jubu", "Male", 50000),
+            new Employee("Alica", "Female", 60000),
+            new Employee("Micky", "Male", 70000),
+            new Employee("Lile", "Female", 55000)
+        ));
+
+        employees.sort(Comparator.comparingDouble(e -> e.salary));
+
+        for (Employee e : employees) {
+            System.out.println(e.name + " - ₹" + e.salary);
+        }
+    }
+}
