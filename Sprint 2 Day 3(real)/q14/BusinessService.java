5a439c8b6d016df8fbceaf51b324a81058f10ccf
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-23 15:58:51 +0530

    Create BusinessService.java

---
+++ Sprint 2 Day 3(real)/q14/BusinessService.java
@@ -0,0 +1,8 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class BusinessService {
+    public void process() {
+        System.out.println("Executing business logic...");
+    }
+}
