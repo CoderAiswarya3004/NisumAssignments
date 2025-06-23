e88f0f1684ebf277e82cc928efad81f8a58e6fc3
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-23 22:57:59 +0530

    Create User.java

---
+++ Sprint 2 Day 4(updated)/q9/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private int id;
+    private String name;
+
+    // Getters and setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
