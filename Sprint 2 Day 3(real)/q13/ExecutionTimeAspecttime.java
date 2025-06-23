100f643471ff2f74be3d4fc3c1eb12ddd52bdfe5
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-23 15:15:27 +0530

    Create ExecutionTimeAspecttime.java

---
+++ Sprint 2 Day 3(real)/q13/ExecutionTimeAspecttime.java
@@ -0,0 +1,22 @@
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionTimeAspect {
+
+    @Around("execution(* BusinessService.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();  // actual method call
+
+        long end = System.currentTimeMillis();
+        System.out.println("Method " + joinPoint.getSignature() +
+                           " executed in " + (end - start) + "ms");
+        return result;
+    }
+}
+
