f86a99447174e6d69381c591a0298f963fbf0303
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-23 14:31:33 +0530

    Create SpellChecker.java

---
+++ Sprint 2 Day 3(real)/q6/SpellChecker.java
@@ -0,0 +1,9 @@
+public class SpellChecker {
+    public SpellChecker() {
+        System.out.println("SpellChecker bean created.");
+    }
+
+    public void checkSpelling() {
+        System.out.println("Spell check is being performed...");
+    }
+}
