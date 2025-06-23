c3a1e3638c3799980bc71bbf9c113d1fb41d1d2f
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-23 21:15:02 +0530

    Create WebInitializer.java

---
+++ Sprint 2 Day 4(updated)/q2/WebInitializer.java
@@ -0,0 +1,19 @@
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null; // All config is in root
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };  // Map all requests to DispatcherServlet
+    }
+}
