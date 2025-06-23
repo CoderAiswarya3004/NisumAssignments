56d83efa05db62df11c2ac12a39fb9dd18e4133c
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-23 21:15:29 +0530

    Update WebInitializer.java

--- Sprint 2 Day 4(updated)/q2/WebInitializer.java
+++ Sprint 2 Day 4(updated)/q2/WebInitializer.java
@@ -9,11 +9,11 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return null; // All config is in root
+        return null; 
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] { "/" };  // Map all requests to DispatcherServlet
+        return new String[] { "/" };  
     }
 }
