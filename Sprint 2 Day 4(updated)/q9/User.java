a6284da3f4ffe13de4c0434e9a4f0518ce140e85
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-23 22:58:15 +0530

    Update User.java

--- Sprint 2 Day 4(updated)/q9/User.java
+++ Sprint 2 Day 4(updated)/q9/User.java
@@ -2,7 +2,6 @@ public class User {
     private int id;
     private String name;
 
-    // Getters and setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
