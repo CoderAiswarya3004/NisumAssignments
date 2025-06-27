0b4634dfc258999072a205890fe1792a75751372
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-27 21:29:47 +0530

    Create q7.java

---
+++ Sprint 3 Day 1/q7/q7.java
@@ -0,0 +1,25 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GlobalExceptionLogger {
+
+    public static void main(String[] args) {
+
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            try (PrintWriter writer = new PrintWriter(new FileWriter("error_log.txt", true))) {
+                writer.println("Exception in thread \"" + thread.getName() + "\": " + throwable);
+                for (StackTraceElement elem : throwable.getStackTrace()) {
+                    writer.println("\tat " + elem);
+                }
+                System.out.println("Logged unhandled exception to file.");
+            } catch (IOException e) {
+                System.out.println("Failed to log exception: " + e.getMessage());
+            }
+        });
+
+        System.out.println("This will throw an exception:");
+        int result = 10 / 0; 
+        System.out.println("End of program.");
+    }
+}
