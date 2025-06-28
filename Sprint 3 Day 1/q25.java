2988c87483e49135be1508d33af3ac9a2437501b
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-06-28 21:02:45 +0530

    Update q25.java

--- Sprint 3 Day 1/q25.java
+++ Sprint 3 Day 1/q25.java
@@ -1,6 +1,5 @@
 class Counter {
     int count = 0;
-
     public void increment() {
         count++;
     }
@@ -8,26 +7,24 @@ class Counter {
 
 class CounterSync {
     int count = 0;
-
     public synchronized void increment() {
         count++;
     }
 }
 
 public class CounterIncrementQ25 {
-
     public static void main(String[] args) throws InterruptedException {
-        // Without synchronization
+        
         Counter unsyncCounter = new Counter();
 
         Thread t1 = new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
+            for (int i = 0; i < 500; i++) {
                 unsyncCounter.increment();
             }
         });
 
         Thread t2 = new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
+            for (int i = 0; i < 500; i++) {
                 unsyncCounter.increment();
             }
         });
@@ -37,16 +34,18 @@ public class CounterIncrementQ25 {
         t1.join();
         t2.join();
 
-        System.out.println("Without Synchronization: " + unsyncCounter.count);  
+        System.out.println("Without Synchronization: " + unsyncCounter.count); // May be < 1000
+
         CounterSync syncCounter = new CounterSync();
 
         Thread t3 = new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
+            for (int i = 0; i < 500; i++) {
                 syncCounter.increment();
             }
         });
+
         Thread t4 = new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
+            for (int i = 0; i < 500; i++) {
                 syncCounter.increment();
             }
         });
@@ -54,6 +53,6 @@ public class CounterIncrementQ25 {
         t4.start();
         t3.join();
         t4.join();
-        System.out.println("With Synchronization: " + syncCounter.count);  
+        System.out.println("With Synchronization: " + syncCounter.count); 
     }
 }
