d047e994ee6e753c171010cd88bc30e1071f2461
CoderAiswarya3004 <160422188+CoderAiswarya3004@users.noreply.github.com>
2025-07-01 12:30:56 +0530

    Create NisumAssignmentsApplication.java

---
+++ Sprint 3 Day 3/q1/NisumAssignmentsApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumAssignmentsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NisumAssignmentsApplication.class, args);
+    }
+}
